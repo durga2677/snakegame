0fae0ac0d99f0b065b5372d77e22cabf93b673e8
durga2677 <125868369+durga2677@users.noreply.github.com>
2023-05-24 09:07:51 +0530

    Add files via upload

---
+++ sapling swing/swingLearning.java
@@ -0,0 +1,13 @@
+
+import javax.swing.*;
+import java.awt.*;
+public class swingLearning {
+    public static void main(String[] args) {
+        
+    JFrame frame = new JFrame("Introduction");
+    frame.setBounds(200,200,500,500);
+    frame.getContentPane().setBackground(Color.yellow);
+    frame.setVisible(true);
+    }
+    
+}
